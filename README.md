c49346047faca8883d63ff404b37d8e529002d20
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-05-14 23:28:10 -0400

    updated keymap and added svg

---
+++ README.md
@@ -0,0 +1,1 @@
+![keymap](./silakka54_keymap.svg)
--- keymap.c
+++ keymap.c
@@ -3,21 +3,50 @@
 
 #include QMK_KEYBOARD_H
 
+
+enum custom_keycodes {
+    CKC_COPY = SAFE_RANGE,
+    CKC_PASTE,
+    JIGGLE,
+};
+
+#define MEH_SPACE MT(MOD_MEH, KC_SPACE)
+#define CKC_CTRL MT(MOD_LCTL,KC_ESC)
+
+// Some helper C macros
+    #define GENERAL_MODIFIER_KEY_DELAY_MS 20
+    #define GENERAL_KEY_ACTION_DELAY_MS   50
+
+    #define KEY_MODIFIER_ACTION(keycode, modifier) \
+        SS_DOWN(modifier) \
+        SS_DELAY(GENERAL_MODIFIER_KEY_DELAY_MS) \
+        SS_TAP(keycode) \
+        SS_DELAY(GENERAL_KEY_ACTION_DELAY_MS) \
+        SS_UP(modifier) \
+        SS_DELAY(GENERAL_MODIFIER_KEY_DELAY_MS)
+
+    #define KEY_CTRL_ACTION(keycode) \
+        KEY_MODIFIER_ACTION(keycode,X_LCTL)
+
+    #define KEY_APPLE_KEY_ACTION(keycode) \
+        KEY_MODIFIER_ACTION(keycode,X_LCMD)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, MO(1), KC_SPC,           KC_ENT,  KC_RCTL,  KC_RALT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+        CKC_CTRL,KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+                                            KC_LGUI, MO(1), KC_BSPC,              MEH_SPACE,  QK_REP,  KC_RALT
     ),
     [1] = LAYOUT(
         KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_DEL,  KC_F12,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
+        KC_TRNS,  KC_TRNS, KC_TRNS, CKC_COPY,KC_TRNS, CKC_PASTE,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                            KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  MO(3),  KC_TRNS
     ),
     [2] = LAYOUT(
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -30,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
     [4] = LAYOUT(
@@ -62,3 +91,105 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     )
 };
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    if (record->event.pressed) {
+        static deferred_token token = INVALID_DEFERRED_TOKEN;
+        static report_mouse_t report = {0};
+        if (token) {
+            // If jiggler is currently running, stop when any key is pressed.
+            cancel_deferred_exec(token);
+            token = INVALID_DEFERRED_TOKEN;
+            report = (report_mouse_t){};  // Clear the mouse.
+            host_mouse_send(&report);
+
+        } else if (keycode == JIGGLE) {
+
+            uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
+                // Deltas to move in a circle of radius 20 pixels over 32 frames.
+                static const int8_t deltas[32] = {
+                    0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+                    0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
+                static uint8_t phase = 0;
+                // Get x delta from table and y delta by rotating a quarter cycle.
+                report.x = deltas[phase];
+                report.y = deltas[(phase + 8) & 31];
+                phase = (phase + 1) & 31;
+                host_mouse_send(&report);
+                return 16;  // Call the callback every 16 ms.
+            }
+            token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
+        }
+    }
+
+    switch (keycode) {
+        case CKC_COPY:
+            if (record->event.pressed) {
+                #if defined(OS_DETECTION_ENABLE)
+                os_variant_t host = detected_host_os();
+                if (host == OS_MACOS || host == OS_IOS) {
+                    // Mac: Cmd + C
+                    SEND_STRING(KEY_APPLE_KEY_ACTION(X_C));
+                }
+                else {
+                    // Linux, Windows, etc.: Ctrl + C
+                    SEND_STRING(KEY_CTRL_ACTION(X_C));
+                }
+                #endif
+            }
+            break;
+
+        case CKC_PASTE:
+            if (record->event.pressed) {
+                #if defined(OS_DETECTION_ENABLE)
+                os_variant_t host = detected_host_os();
+                if (host == OS_MACOS || host == OS_IOS) {
+                    // Mac: Cmd + V
+                    SEND_STRING(KEY_APPLE_KEY_ACTION(X_V));
+                }
+                else {
+                    // Linux, Windows, etc.: Ctrl + V
+                    SEND_STRING(KEY_CTRL_ACTION(X_V));
+                }
+                #endif
+            }
+            break;
+
+        // your code here
+        // QMK: shift + backspace = delete
+        case KC_BSPC: {
+            static uint16_t registered_key = KC_NO;
+            if (record->event.pressed) {
+                const uint8_t mods = get_mods();
+#ifndef NO_ACTION_ONESHOT
+                uint8_t shift_mods = (mods | get_oneshot_mods()) & MOD_MASK_SHIFT;
+#else
+                uint8_t shift_mods = mods & MOD_MASK_SHIFT;
+#endif
+                if (shift_mods) {
+                    registered_key = KC_DEL;
+
+                if (shift_mods != MOD_MASK_SHIFT) {
+#ifndef NO_ACTION_ONESHOT
+                    del_oneshot_mods(MOD_MASK_SHIFT);
+#endif
+                    unregister_mods(MOD_MASK_SHIFT);
+                }
+            } else {
+                registered_key = KC_BSPC;
+            }
+
+            register_code(registered_key);
+            set_mods(mods);
+        } else {
+            unregister_code(registered_key);
+        }
+      } return false;
+
+    // Other macros...
+    }
+    return true;
+}
+
