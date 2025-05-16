24798eb0e826747d41c1738fedda1a156ac29985
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-05-16 10:42:08 -0400

    updated symbol layer

--- keymap.c
+++ keymap.c
@@ -6,12 +6,25 @@
 
 enum custom_keycodes {
     CKC_COPY = SAFE_RANGE,
+    CKC_UPDIR,
     CKC_PASTE,
+    CKC_SCOPE,
+    CKC_CUT,
     JIGGLE,
+    SELLINE,
+    SELALL,
+    SMTD_KEYCODES_BEGIN,
+    CKC_R, // reads as C(ustom) + KC_A
+    CKC_I,
+    SMTD_KEYCODES_END,
 };
+
+#include "sm_td/sm_td.h"
 
 #define MEH_SPACE MT(MOD_MEH, KC_SPACE)
 #define CKC_CTRL MT(MOD_LCTL,KC_ESC)
+#define CKC_SLSH LSFT_T(KC_SLSH)
+#define CUSTOM_TAP_DELAY 10
 
 // Some helper C macros
     #define GENERAL_MODIFIER_KEY_DELAY_MS 20
@@ -37,22 +50,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        CKC_CTRL,KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        CKC_CTRL,KC_A,    CKC_R,    KC_S,    KC_T,    KC_G,                              KC_M,    KC_N,    KC_E,    CKC_I,   KC_O,    KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                               KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                             KC_LGUI, MO(1), KC_BSPC,              MEH_SPACE,  QK_REP,  KC_RALT
     ),
     [1] = LAYOUT(
         KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_DEL,  KC_F12,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
-        KC_TRNS,  KC_TRNS, KC_TRNS, CKC_COPY,KC_TRNS, CKC_PASTE,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  SELALL,  SELLINE, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
+        KC_TRNS,  KC_TRNS, CKC_CUT, CKC_COPY,KC_TRNS, CKC_PASTE,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  MO(3),  KC_TRNS
     ),
     [2] = LAYOUT(
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_GRV,  KC_LABK, KC_RABK, KC_DQUO, KC_DOT,                             KC_AMPR,  CKC_SCOPE, KC_LBRC, KC_RBRC, KC_PERC, KC_TRNS,
+        KC_TRNS,  KC_EXLM, KC_MINS, KC_PLUS, KC_EQL,  KC_HASH,                            KC_PIPE,  KC_COLN, KC_LPRN, KC_RPRN, KC_QUES, KC_TRNS,
+        KC_TRNS,  KC_CIRC, CKC_SLSH,KC_ASTR, KC_BSLS, CKC_UPDIR,                          KC_TILD,  KC_DLR,  KC_LCBR, KC_RCBR, KC_AT, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
     [3] = LAYOUT(
@@ -129,7 +142,70 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
 
-        // your code here
+        case CKC_CUT:
+            if (record->event.pressed) {
+                #if defined(OS_DETECTION_ENABLE)
+                os_variant_t host = detected_host_os();
+                if (host == OS_MACOS || host == OS_IOS) {
+                    // Mac: Cmd + X
+                    SEND_STRING(KEY_APPLE_KEY_ACTION(X_X));
+                }
+                else {
+                    // Linux, Windows, etc.: Ctrl + X
+                    SEND_STRING(KEY_CTRL_ACTION(X_X));
+                }
+                #endif
+            }
+            break;
+
+        case SELALL:
+            if (record->event.pressed) {
+                #if defined(OS_DETECTION_ENABLE)
+                os_variant_t host = detected_host_os();
+                if (host == OS_MACOS || host == OS_IOS) {
+                    // Mac: Cmd+A
+                    SEND_STRING(KEY_APPLE_KEY_ACTION(X_A));
+                } else {
+                    // Windows/Linux: Ctrl+A
+                    SEND_STRING(KEY_CTRL_ACTION(X_A));
+                }
+                #else
+                // Default to Windows/Linux if OS detection is not enabled
+                SEND_STRING(KEY_CTRL_ACTION(X_A));
+                #endif
+            }
+            break;
+
+        case SELLINE:
+            if (record->event.pressed) {
+                #if defined(OS_DETECTION_ENABLE)
+                os_variant_t host = detected_host_os();
+                if (host == OS_MACOS || host == OS_IOS) {
+                    // Mac: Cmd+Left, then Cmd+Shift+Right
+                    SEND_STRING(SS_LGUI(SS_TAP(X_LEFT)) SS_DELAY(CUSTOM_TAP_DELAY) SS_LGUI(SS_LSFT(SS_TAP(X_RGHT))));
+                } else {
+                    // Windows/Linux: Home, then Shift+End
+                    SEND_STRING(SS_TAP(X_HOME) SS_DELAY(CUSTOM_TAP_DELAY) SS_LSFT(SS_TAP(X_END)));
+                }
+                #else
+                // Default to Windows/Linux if OS detection is not enabled
+                SEND_STRING(SS_TAP(X_HOME) SS_DELAY(CUSTOM_TAP_DELAY) SS_LSFT(SS_TAP(X_END)));
+                #endif
+            }
+            break;
+
+        case CKC_UPDIR:
+            if (record->event.pressed) {
+                SEND_STRING("../");
+            }
+            break;
+
+        case CKC_SCOPE:
+            if (record->event.pressed) {
+                SEND_STRING("::");
+            }
+            break;
+
         // QMK: shift + backspace = delete
         case KC_BSPC: {
             static uint16_t registered_key = KC_NO;
@@ -165,3 +241,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
+    switch (keycode) {
+        SMTD_LT(CKC_R, KC_GRV, 2) // SYM layer
+        SMTD_LT(CKC_I, KC_TAB, 2) // SYM layer
+    }
+}
