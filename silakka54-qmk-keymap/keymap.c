eaee32ff8a41773574f66e1b40f632395848250c
Morphy Kuffour <morpkuff@gmail.com>
2026-04-08 07:49:40 -0400

    pre-flash optimizations
    - LTO_ENABLE = yes: link-time optimization, reduces binary size ~10-20%
    - DYNAMIC_KEYMAP_LAYER_COUNT 8 → 4: only 4 layers defined, frees EEPROM
    - Move QK_BOOT from layer 3 (QWERTY/vim normal) to layer 1 bottom-right:
      prevents accidental bootloader entry during normal vim use
    - Place JIGGLE on layer 1 bottom-left: was compiled in but unreachable
    - Fix KC_BSPC handler indentation: inner if was visually ambiguous

--- silakka54-qmk-keymap/keymap.c
+++ silakka54-qmk-keymap/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_DEL,  KC_F12,
         KC_TRNS,      SELALL,  SELLINE, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
-        KC_TRNS,      KC_TRNS, CKC_CUT, CKC_COPY,KC_TRNS, CKC_PASTE,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,      JIGGLE,  CKC_CUT, CKC_COPY,KC_TRNS, CKC_PASTE,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
                                                 KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  MO(3),  FORCE_QWERTY
     ),
     [2] = LAYOUT(
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_TRNS,
         KC_TRNS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_TRNS,
-        KC_TRNS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, QK_BOOT,
+        KC_TRNS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
                                         FORCE_COLEMAK, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     )
 };
@@ -255,23 +255,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
                 if (shift_mods) {
                     registered_key = KC_DEL;
-
-                if (shift_mods != MOD_MASK_SHIFT) {
+                    if (shift_mods != MOD_MASK_SHIFT) {
 #ifndef NO_ACTION_ONESHOT
-                    del_oneshot_mods(MOD_MASK_SHIFT);
+                        del_oneshot_mods(MOD_MASK_SHIFT);
 #endif
-                    unregister_mods(MOD_MASK_SHIFT);
+                        unregister_mods(MOD_MASK_SHIFT);
+                    }
+                } else {
+                    registered_key = KC_BSPC;
                 }
+                register_code(registered_key);
+                set_mods(mods);
             } else {
-                registered_key = KC_BSPC;
+                unregister_code(registered_key);
             }
-
-            register_code(registered_key);
-            set_mods(mods);
-        } else {
-            unregister_code(registered_key);
-        }
-      } return false;
+        } return false;
 
     // Other macros...
     }
